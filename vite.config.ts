224d80343a68a3491d8598261c2b83117b6e4e05
GitPio <elissakwizera2008@gmail.com>
2026-08-25 15:39:49 +0200

    tanstack adding

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    tanstackRouter(), // Must come before react()
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
   server: { port: 3000 },
 });
